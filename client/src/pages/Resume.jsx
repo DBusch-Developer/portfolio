4ddf233a03edec6abf5fd6bb931f4879c475f8cc
unknown <buschdiana007@gmail.com>
2025-06-02 09:31:20 -0700

    Adds updated resume for download.

--- client/src/pages/Resume.jsx
+++ client/src/pages/Resume.jsx
@@ -1,4 +1,4 @@
-import pdfdoc from "../assets/Resume_Diana_Busch.pdf";
+import pdfdoc from "../assets/Diana-Busch-Resume.pdf";
 
 const Resume = () => {
   return (
@@ -64,7 +64,6 @@ const Resume = () => {
                 d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
               />
             </svg>
-           
           </div>
           <h1 className="text-3xl font-bold pl-5">Experience</h1>
         </div>
